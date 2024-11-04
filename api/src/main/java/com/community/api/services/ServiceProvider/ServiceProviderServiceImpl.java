66b90aac1e847bcd56ad5b31a526286e142de0b4
simran2519 <simranjit.kaur@avisoft.io>
2024-11-04 14:00:16 -0800

    infra-score-bug

--- api/src/main/java/com/community/api/services/ServiceProvider/ServiceProviderServiceImpl.java
+++ api/src/main/java/com/community/api/services/ServiceProvider/ServiceProviderServiceImpl.java
@@ -327,7 +327,7 @@ public class ServiceProviderServiceImpl implements ServiceProviderService {
 
             if(existingServiceProvider.getType().equalsIgnoreCase("INDIVIDUAL"))
             {
-                if(updates.containsKey("infra_list"))
+                if (updates.containsKey("infra_list") && (updates.get("infra_list") instanceof List) && !((List<?>) updates.get("infra_list")).isEmpty())
                 {
                     List<ServiceProviderInfra> infrastructures=existingServiceProvider.getInfra();
                     int totalInfras=infrastructures.size();
@@ -343,7 +343,7 @@ public class ServiceProviderServiceImpl implements ServiceProviderService {
                             scoringCriteriaToMap=null;
                         }
                     }
-                    if(totalInfras>=2 && totalInfras<=4)
+                    else if(totalInfras>=2 && totalInfras<=4)
                     {
                         scoringCriteriaToMap=traverseListOfScoringCriteria(14L,scoringCriteriaList,existingServiceProvider);
                         if(scoringCriteriaToMap==null)
@@ -355,7 +355,7 @@ public class ServiceProviderServiceImpl implements ServiceProviderService {
                             scoringCriteriaToMap=null;
                         }
                     }
-                    if(totalInfras==1)
+                    else if(totalInfras==1)
                     {
                         scoringCriteriaToMap=traverseListOfScoringCriteria(15L,scoringCriteriaList,existingServiceProvider);
                         if(scoringCriteriaToMap==null)
@@ -367,17 +367,14 @@ public class ServiceProviderServiceImpl implements ServiceProviderService {
                             scoringCriteriaToMap=null;
                         }
                     }
-                    if(totalInfras==0)
-                    {
-                        scoringCriteriaToMap=traverseListOfScoringCriteria(16L,scoringCriteriaList,existingServiceProvider);
-                        if(scoringCriteriaToMap==null)
-                        {
-                            return ResponseService.generateErrorResponse("Scoring Criteria is not found for Infra Score", HttpStatus.BAD_REQUEST);
-                        }
-                        else {
-                            existingServiceProvider.setInfraScore(scoringCriteriaToMap.getScore());
-                            scoringCriteriaToMap=null;
-                        }
+                }
+                else if (updates.containsKey("infra_list") && (updates.get("infra_list") instanceof List) && ((List<?>) updates.get("infra_list")).isEmpty()) {
+                    scoringCriteriaToMap = traverseListOfScoringCriteria(16L, scoringCriteriaList, existingServiceProvider);
+                    if (scoringCriteriaToMap == null) {
+                        return ResponseService.generateErrorResponse("Scoring Criteria is not found for Infra Score", HttpStatus.BAD_REQUEST);
+                    } else {
+                        existingServiceProvider.setInfraScore(scoringCriteriaToMap.getScore());
+                        scoringCriteriaToMap=null;
                     }
                 }
 
