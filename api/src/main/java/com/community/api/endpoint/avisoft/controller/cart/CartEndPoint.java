fdeb6face1faa29326e67a694749820392a65fa3
simran2519 <simranjit.kaur@avisoft.io>
2024-11-19 11:28:58 -0800

    exception is added for invalid gender

--- api/src/main/java/com/community/api/endpoint/avisoft/controller/cart/CartEndPoint.java
+++ api/src/main/java/com/community/api/endpoint/avisoft/controller/cart/CartEndPoint.java
@@ -196,10 +196,11 @@ public class CartEndPoint extends BaseEndpoint {
                     customer.getEmailAddress() == null ||
                     customCustomer.getCategory() == null ||
                     customer.getUsername() == null ||
-                    customer.getPassword() == null)
+                    customer.getPassword() == null||
+                    customCustomer.getGender()==null)
             {
                 return ResponseService.generateErrorResponse(
-                        "All fields must be completed: First Name, Last Name, Primary Email, Username, Password, and Category are required before setting up the cart.",
+                        "All fields must be completed: First Name, Last Name, Primary Email, Username, Password, Gender and Category are required before setting up the cart.",
                         HttpStatus.BAD_REQUEST
                 );
             }
@@ -220,6 +221,10 @@ public class CartEndPoint extends BaseEndpoint {
                 return ResponseService.generateErrorResponse("Invalid Category",HttpStatus.INTERNAL_SERVER_ERROR);
             if(reserveCategoryService.getReserveCategoryFee(productId,reserveCategoryId)==null)
                 return ResponseService.generateErrorResponse("Cannot add product to cart :Fee not specified for your category",HttpStatus.UNPROCESSABLE_ENTITY);
+            if(!customCustomer.getGender().equalsIgnoreCase(customProduct.getGenderSpecific().getGenderName()))
+            {
+                return ResponseService.generateErrorResponse("Cannot add product to cart: Product not specified for gender "+ customCustomer.getGender(),HttpStatus.BAD_REQUEST);
+            }
             /*if(productReserveCategoryFeePostRefService.getCustomProductReserveCategoryFeePostRefByProductIdAndReserveCategoryId(product.getId(),.getFee()==null)
             {
 
