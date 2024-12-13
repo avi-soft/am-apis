611805dbaaf22b1a6568264819ebe26280f4607d
kshtjvats <kshitij.vats@avisoft.io>
2024-12-13 13:00:15 +0530

    pulled dev

--- api/src/main/java/com/community/api/entity/CustomerReferrer.java
+++ api/src/main/java/com/community/api/entity/CustomerReferrer.java
@@ -1,6 +1,7 @@
 package com.community.api.entity;
 
 import com.community.api.endpoint.serviceProvider.ServiceProviderEntity;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,6 +22,7 @@ public class CustomerReferrer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @JsonManagedReference(value = "referrer-customer")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "customer_id")
     private CustomCustomer customer;
