881778646debd4d77043f613b269516b221f1ceb
raman <ramanmalik165@gmail.comn>
2024-12-20 08:20:25 +0530

    advertisement post api

---
+++ api/src/main/java/com/community/api/dto/AdvertisementWrapper.java
@@ -0,0 +1,98 @@
+package com.community.api.dto;
+
+import com.community.api.entity.Advertisement;
+import com.community.api.entity.Role;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.broadleafcommerce.common.rest.api.wrapper.APIWrapper;
+import org.broadleafcommerce.common.rest.api.wrapper.BaseWrapper;
+import org.broadleafcommerce.core.catalog.domain.Category;
+import org.broadleafcommerce.core.catalog.domain.CategoryImpl;
+import org.broadleafcommerce.core.catalog.domain.Product;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+public class AdvertisementWrapper extends BaseWrapper implements APIWrapper<Advertisement> {
+
+    @JsonProperty("advertisement_id")
+    private Long advertisementId;
+
+    @JsonProperty("number")
+    private Long number;
+
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("description")
+    private String description;
+
+    @JsonProperty("created_date")
+    private Date createdDate;
+
+    @JsonProperty("creator_user_id")
+    private Long userId;
+
+    @JsonProperty("creator_role_id")
+    private Role creatorRole;
+
+    @JsonProperty("modified_date")
+    private Date modifiedDate;
+
+    @JsonProperty("modifier_user_id")
+    private Long modifierId;
+
+    @JsonProperty("modifier_role_id")
+    private Role modifierRole;
+
+    @JsonProperty("active_start_date")
+    private Date activeStartDate;
+
+    @JsonProperty("active_end_date")
+    private Date activeEndDate;
+
+    @JsonProperty("url")
+    private String url;
+
+    @JsonProperty("category")
+    private CustomCategoryWrapper category;
+
+
+    @Override
+    public void wrapDetails(Advertisement advertisement, HttpServletRequest httpServletRequest) {
+        this.advertisementId = advertisement.getAdvertisementId();
+        this.title = advertisement.getTitle();
+        this.number = advertisement.getNumber();
+        this.description = advertisement.getDescription();
+        this.url = advertisement.getUrl();
+        this.createdDate = advertisement.getCreatedDate();
+        this.activeStartDate = advertisement.getActiveStartDate();
+        this.activeEndDate = advertisement.getActiveEndDate();
+        this.modifiedDate = advertisement.getModifiedDate();
+        this.userId = advertisement.getUserId();
+        this.creatorRole = advertisement.getCreatorRole();
+        this.modifierId = advertisement.getModifierId();
+        this.modifierRole = advertisement.getModifierRole();
+        this.modifiedDate = advertisement.getModifiedDate();
+        CustomCategoryWrapper categoryWrapper = new CustomCategoryWrapper();
+        categoryWrapper.wrapSummary((Category) advertisement.getCategory(), null);
+        this.category = categoryWrapper;
+    }
+
+    @Override
+    public void wrapSummary(Advertisement advertisement, HttpServletRequest httpServletRequest) {
+
+    }
+}
---
+++ api/src/main/java/com/community/api/services/AdvertisementService.java
@@ -0,0 +1,132 @@
+package com.community.api.services;
+
+import com.community.api.dto.AddAdvertisementDto;
+import com.community.api.entity.Advertisement;
+import com.community.api.entity.Role;
+import com.community.api.services.exception.ExceptionHandlingService;
+import org.broadleafcommerce.core.catalog.domain.Category;
+import org.broadleafcommerce.core.catalog.domain.CategoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Service
+public class AdvertisementService {
+
+    protected SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    @Autowired
+    ExceptionHandlingService exceptionHandlingService;
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public void validateAdvertisement(AddAdvertisementDto addAdvertisementDto) throws Exception {
+        try {
+            if(addAdvertisementDto.getTitle() == null || addAdvertisementDto.getTitle().trim().isEmpty()) {
+                throw new IllegalArgumentException("Advertisement Title cannot be null or empty");
+            }
+            addAdvertisementDto.setTitle(addAdvertisementDto.getTitle().trim());
+
+            if(addAdvertisementDto.getDescription() != null) {
+                if (addAdvertisementDto.getDescription().trim().isEmpty()) {
+                    throw new IllegalArgumentException("Advertisement Description cannot be Empty");
+                }
+                addAdvertisementDto.setDescription(addAdvertisementDto.getDescription().trim());
+            }
+
+            if(addAdvertisementDto.getUrl() == null || addAdvertisementDto.getUrl().trim().isEmpty()) {
+                throw new IllegalArgumentException("Advertisement Url cannot be null or empty");
+            }
+            addAdvertisementDto.setUrl(addAdvertisementDto.getUrl().trim());
+
+            if(addAdvertisementDto.getActiveStartDate() == null) {
+                throw new IllegalArgumentException("Active Start Date is required");
+            }
+            String formattedDate = dateFormat.format(addAdvertisementDto.getActiveStartDate());
+            dateFormat.parse(formattedDate); // Convert formatted date string back to Date
+
+            if(addAdvertisementDto.getActiveEndDate() == null) {
+                addAdvertisementDto.setActiveEndDate(addAdvertisementDto.getActiveStartDate());
+            } else {
+                formattedDate = dateFormat.format(addAdvertisementDto.getActiveEndDate());
+                dateFormat.parse(formattedDate); // Convert formatted date string back to Date
+            }
+
+            if (addAdvertisementDto.getActiveEndDate().before(addAdvertisementDto.getActiveStartDate())) {
+                throw new IllegalArgumentException("Active end date cannot be before of active start date");
+            }
+
+        } catch (IllegalArgumentException illegalArgumentException) {
+            exceptionHandlingService.handleException(illegalArgumentException);
+            throw new IllegalArgumentException(illegalArgumentException.getMessage() + "\n");
+        } catch (ParseException parseException) {
+            exceptionHandlingService.handleException(parseException);
+            throw new ParseException(parseException.getMessage() + "\n", parseException.getErrorOffset());
+        } catch (Exception exception) {
+            exceptionHandlingService.handleException(exception);
+            throw new Exception(exception.getMessage() + "\n");
+        }
+    }
+
+    public Advertisement saveAdvertisement (AddAdvertisementDto addAdvertisementDto, Long creatorUserId, Role role, CategoryImpl category) {
+        try {
+
+            /*// Start building the SQL query
+            StringBuilder sql = new StringBuilder("INSERT INTO advertisement (title, number, creator_user_id, creator_role_id, created_date, active_start_date, active_end_date, url, category_id");
+            StringBuilder values = new StringBuilder("VALUES (:title, :number , :creatorUserId, :role, :currentDate, :url, :categoryId");
+
+            // Dynamically add columns and values based on non-null fields
+            if (addAdvertisementDto.getDescription() != null) {
+                sql.append(", description");
+                values.append(", :description");
+            }
+
+            // Complete the SQL statement
+            sql.append(") ").append(values).append(")");
+
+            String formattedDate = dateFormat.format(new Date());
+            Date createdDate  = dateFormat.parse(formattedDate); // Convert formatted date string back to Date
+            // Create the query
+            var query = entityManager.createNativeQuery(sql.toString())
+                    .setParameter("title", addAdvertisementDto.getTitle())
+                    .setParameter("creatorUserId", creatorUserId)
+                    .setParameter("role", role)
+                    .setParameter("number", addAdvertisementDto.getNumber())
+                    .setParameter("url", addAdvertisementDto.getUrl())
+                    .setParameter("categoryId", category.getId())
+                    .setParameter("currentDate", createdDate);
+
+            // Set parameters conditionally
+            if (addAdvertisementDto.getDescription() != null) {
+                query.setParameter("description", addAdvertisementDto.getDescription());
+            }
+
+            // Execute the update
+            query.executeUpdate();
+*/
+
+            String formattedDate = dateFormat.format(new Date());
+            Date createdDate  = dateFormat.parse(formattedDate); // Convert formatted date string back to Date
+
+            Advertisement advertisement = new Advertisement();
+            advertisement.setTitle(addAdvertisementDto.getTitle());
+            advertisement.setUrl(addAdvertisementDto.getUrl());
+            advertisement.setDescription(addAdvertisementDto.getDescription());
+            advertisement.setNumber(addAdvertisementDto.getNumber());
+            advertisement.setCreatedDate(createdDate);
+            advertisement.setActiveStartDate(addAdvertisementDto.getActiveStartDate());
+            advertisement.setActiveEndDate(addAdvertisementDto.getActiveEndDate());
+            advertisement.setCategory(category);
+
+            return entityManager.merge(advertisement);
+        } catch (Exception e) {
+            exceptionHandlingService.handleException(e);
+            throw new RuntimeException("Failed to save Advertisement: " + e.getMessage(), e);
+        }
+    }
+}
