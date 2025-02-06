89637ab6620c041cfdfa9b08003fe26756ec62e4
Raman <raman.kumar@avisoft>
2025-02-06 16:26:36 +0530

    changes fixed

--- api/src/main/java/com/community/api/component/JwtAuthenticationFilter.java
+++ api/src/main/java/com/community/api/component/JwtAuthenticationFilter.java
@@ -158,6 +158,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private boolean isUnsecuredUri(String requestURI) {
         return requestURI.startsWith("/api/v1/account")
                 || requestURI.startsWith("/api/v1/otp")
+                || requestURI.startsWith("/api/v1/category-custom/get-all-categories")
                 || requestURI.startsWith("/api/v1/test")
                 || requestURI.startsWith("/api/v1/files/avisoftdocument/**")
                 || requestURI.startsWith("/api/v1/files/**")
