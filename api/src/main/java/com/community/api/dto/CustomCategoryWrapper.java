5b9373b2f6c7f0f0d6756c4534f0911e41fe13c8
simran2519 <simranjit.kaur@avisoft.io>
2025-02-17 14:25:51 +0530

    sout is removed

--- api/src/main/java/com/community/api/dto/CustomCategoryWrapper.java
+++ api/src/main/java/com/community/api/dto/CustomCategoryWrapper.java
@@ -72,7 +72,6 @@ public class CustomCategoryWrapper extends BaseWrapper implements APIWrapper<Cat
         this.urlKey = category.getUrlKey();
         this.archived = ((Status) category).getArchived();
         this.products = products;
-        System.out.println(products);
         if (products == null) {
             this.totalProducts = 0;
         } else {
