43c30c84597832c205036416eb48ba42778c78a8
kshtjvats <kshitij.vats@avisoft.io>
2025-03-28 11:14:48 +0530

    fixed transaction bus

--- api/src/main/java/com/community/api/services/PaymentService.java
+++ api/src/main/java/com/community/api/services/PaymentService.java
@@ -34,17 +34,25 @@ public class PaymentService {
 
         System.out.println("date"+firstOfMonth);
 
-        Query queryAmount = entityManager.createNativeQuery("SELECT COALESCE(SUM(e.pending-e.paid), 0) FROM Earnings e WHERE e.date < :inputDate AND e.provider_id = :id AND settled = false");
+        Query queryAmount = entityManager.createNativeQuery("SELECT COALESCE(SUM(e.pending), 0) FROM Earnings e WHERE e.date < :inputDate AND e.provider_id = :id AND settled = false");
         queryAmount.setParameter("inputDate", java.sql.Date.valueOf(firstOfMonth));
         queryAmount.setParameter("id", spId);
         Double lastMonth = (Double) queryAmount.getSingleResult();
-        queryAmount=entityManager.createNativeQuery("SELECT COALESCE(SUM(e.pending-e.paid), 0) FROM Earnings e WHERE e.date >= :inputDate AND e.provider_id = :id AND settled = false");
+        Query queryAmountCarryOver = entityManager.createNativeQuery("SELECT SUM(e.carry_over) FROM Earnings e WHERE e.date < :inputDate AND e.provider_id = :id AND settled = true");
+        queryAmountCarryOver.setParameter("inputDate", java.sql.Date.valueOf(firstOfMonth));
+        queryAmountCarryOver.setParameter("id", spId);
+        Double lastMonthCarryOver = (Double) queryAmountCarryOver.getSingleResult();
+        queryAmount=entityManager.createNativeQuery("SELECT COALESCE(SUM(e.pending), 0) FROM Earnings e WHERE e.date >= :inputDate AND e.provider_id = :id AND settled = false");
         queryAmount.setParameter("inputDate", java.sql.Date.valueOf(firstOfMonth));
         queryAmount.setParameter("id", spId);
         Double thisMonth = (Double) queryAmount.getSingleResult();
+        Query queryAmountCarryOver2 = entityManager.createNativeQuery("SELECT SUM(e.carry_over) FROM Earnings e WHERE e.date >=:inputDate AND e.provider_id = :id AND settled = true");
+        queryAmountCarryOver2.setParameter("inputDate", java.sql.Date.valueOf(firstOfMonth));
+        queryAmountCarryOver2.setParameter("id", spId);
+        Double thisMonthCarryOver = (Double) queryAmountCarryOver2.getSingleResult();
         Map<String,Object> resultMap=new HashMap<>();
-        result[0]=lastMonth;
-        result[1]=thisMonth;
+        result[0]=lastMonth+lastMonthCarryOver;
+        result[1]=thisMonth+thisMonthCarryOver;
         return result;
     }
 }
