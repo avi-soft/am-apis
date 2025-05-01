b26b553f36ddca17b8514f0779b9cc706561bce4
Dharmendra Singh <dharmendra.singh@avisoft.io>
2025-05-01 11:38:50 +0530

    changes in customer endpoint

--- api/src/main/java/com/community/api/endpoint/avisoft/controller/Customer/CustomerEndpoint.java
+++ api/src/main/java/com/community/api/endpoint/avisoft/controller/Customer/CustomerEndpoint.java
@@ -1685,7 +1685,7 @@ public class CustomerEndpoint {
                                     .orElse(null);
 
                             if (existingDocument13 == null) {
-                                 createdDocument = documentStorageService.createDocument(file, documentTypeObj, customCustomer, customerId, role);
+                                 Document createdDocument = documentStorageService.createDocument(file, documentTypeObj, customCustomer, customerId, role);
                                 if (documentTypeObj.getDocument_type_id().equals(13)) {
                                     createdDocument.setOtherDocument(otherDocument);
                                     entityManager.merge(createdDocument);
