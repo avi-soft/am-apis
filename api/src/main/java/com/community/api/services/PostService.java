93620ac5c23385699fb9028fec471f304a0275ef
simran2519 <simranjit.kaur@avisoft.io>
2025-05-26 18:41:15 +0530

    state level category issue resolved

--- api/src/main/java/com/community/api/services/PostService.java
+++ api/src/main/java/com/community/api/services/PostService.java
@@ -954,14 +954,8 @@ public class PostService {
                     catDist.setIsStateLevelCategory(catDto.getIsStateLevelCategory());
 
                     catDist.setStateLevelCategory(catDto.getStateLevelCategory());
-                    CustomReserveCategory category = entityManager.find(CustomReserveCategory.class, 6L);
-                    if (category == null) {
-                        throw new IllegalArgumentException("Category not found with id: " + catDto.getCategoryId());
-                    }
-                    if(category.getReserveCategoryName().equalsIgnoreCase("Others"))
-                    {
-                        catDist.setCategory(category);
-                    }
+                    catDist.setCategory(null);
+
                 }
                 else {
                     CustomReserveCategory category = entityManager.find(CustomReserveCategory.class, catDto.getCategoryId());
@@ -978,10 +972,13 @@ public class PostService {
                     catDist.setFemaleVacancy(catDto.getFemaleVacancy());
                     catDist.setTotalVacancy(catDist.getTotalVacancy());
                 }
-                if(catDto.getCategoryId()!=6&&catDto.getCategoryRunningField()!=null)
-                    throw new IllegalArgumentException("Cannot add running field for category except OTHERS");
-                if(catDto.getCategoryId()==6&&(catDto.getCategoryRunningField()==null||catDto.getCategoryRunningField().trim().isEmpty()))
-                    throw new IllegalArgumentException("Running field required when selecting category : OTHERS");
+                if(catDto.getCategoryId()!=null)
+                {
+                    if(catDto.getCategoryId()!=6&&catDto.getCategoryRunningField()!=null)
+                        throw new IllegalArgumentException("Cannot add running field for category except OTHERS");
+                    if(catDto.getCategoryId()==6&&(catDto.getCategoryRunningField()==null||catDto.getCategoryRunningField().trim().isEmpty()))
+                        throw new IllegalArgumentException("Running field required when selecting category : OTHERS");
+                }
                 catDist.setCategoryRunningField(catDto.getCategoryRunningField());
                 catDist.setCategoryVacancies(catDto.getCategoryVacancies());
 
