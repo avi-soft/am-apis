d90083330cd8aa14506f5dc464d2526a22b191da
kshtjvats <kshitij.vats@avisoft.io>
2025-06-09 17:12:08 +0530

    Added sector

--- api/src/main/java/com/community/api/Repository/ProductEventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.community.api.Repository;
-
-import com.community.api.entity.ProductEvent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductEventRepository extends JpaRepository<ProductEvent,Long> {
-}
