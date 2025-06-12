cf1ab2816838c36a5715f1a56eeee274ff50e4a8
Chhaya-20 <chhaya.chugh@avisoft.io>
2025-06-12 13:26:41 +0530

    added some sql file of re-ranking stored procedures

---
+++ api/src/main/resources/re-ranking/8_get_service_providers_for_subsequent_ranking.sql
@@ -0,0 +1,28 @@
+CREATE OR REPLACE PROCEDURE public.get_service_providers_not_admin_overridden_and_eligibe_order_by_ticket_completed(
+    INOUT service_provider_ids BIGINT[]
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    sp_id BIGINT;
+BEGIN
+
+	RAISE NOTICE '8. Get Service Providers which are not Admin Overridden and Eligible and are approved order by ticket completed';
+    service_provider_ids := '{}';  -- Initialize empty array
+
+    FOR sp_id IN
+        SELECT service_provider_id
+        FROM public.service_provider
+        WHERE is_admin_overridden = false
+          AND (is_eligible_for_re_ranking = true)
+          AND role = 4
+          and approved = true
+        ORDER BY COALESCE(ticket_completed, 0) desc,
+        date_joined ASC  -- or DESC based on your preference
+    LOOP
+        service_provider_ids := array_append(service_provider_ids, sp_id);
+    END LOOP;
+
+    RAISE NOTICE 'Total SPs fetched for subsequent ranking: %', array_length(service_provider_ids, 1);
+   END;
+$$;
