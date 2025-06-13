aca5b89b138173016d55b9aa3a72eefcd953124c
kshtjvats <kshitij.vats@avisoft.io>
2025-06-13 11:02:02 +0530

    Fixed rollback error

--- api/src/main/java/com/community/api/endpoint/avisoft/controller/GlobalException.java
+++ api/src/main/java/com/community/api/endpoint/avisoft/controller/GlobalException.java
@@ -16,6 +16,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
+import javax.jms.TransactionRolledBackException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -74,7 +75,10 @@ public class GlobalException {
     public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex, WebRequest request) {
         return generateErrorResponse("Invalid argument", HttpStatus.BAD_REQUEST,ex.getMessage());
     }
-
+    @ExceptionHandler(value = {TransactionRolledBackException.class})
+    public ResponseEntity<ErrorResponse> handleTransactionRollback(TransactionRolledBackException ex, WebRequest request) {
+        return generateErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST,ex.getMessage());
+    }
     @ExceptionHandler(value = { RuntimeException.class })
     public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException ex, WebRequest request) {
         return generateErrorResponse("Runtime exception " , HttpStatus.INTERNAL_SERVER_ERROR,ex.getMessage());
