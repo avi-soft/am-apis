339ea18668e28df19156e31714d072c671586d0d
simran2519 <simranjit.kaur@avisoft.io>
2025-06-16 16:30:31 +0530

    changes done

--- api/src/main/java/com/community/api/services/DocumentStorageService.java
+++ api/src/main/java/com/community/api/services/DocumentStorageService.java
@@ -1180,7 +1180,6 @@ public class DocumentStorageService {
 
                 // Append qualification_detail_id condition if provided
                 if (documentTypeObj.getDocument_type_id().equals(Constant.DOCUMENT_TYPE_MARK_SHEET_ID) && qualificationDetailId != null) {
-                    System.out.println("hiii");
                     isQualificationDocumentToDelete = true;
                     archiveIdQuery += " AND qualification_detail_id = :qualificationDetailId";
                     archiveQuery += " AND qualification_detail_id = :qualificationDetailId";
@@ -1197,7 +1196,6 @@ public class DocumentStorageService {
                 query.setParameter("userId", customerId);
                 query.setParameter("documentTypeId", fileType);
                 if (isQualificationDocumentToDelete) {
-                    System.out.println("hii");
                     query.setParameter("qualificationDetailId", qualificationDetailId);
                 }
                 if (isOtherDocumentToDelete) {
@@ -1207,9 +1205,8 @@ public class DocumentStorageService {
                 if (resultList.isEmpty()) {
                     throw new IllegalArgumentException("No document found for deletion");
                 }
-                System.out.println(resultList);
+
                 BigInteger id = (BigInteger) resultList.get(0);
-                System.out.println(id);
                 query = entityManager.createNativeQuery(archiveQuery);
                 query.setParameter("userId", customerId);
                 query.setParameter("documentTypeId", fileType);
@@ -1256,7 +1253,6 @@ public class DocumentStorageService {
                                     if (documentToDelete.getDocumentId().equals(serviceProviderDocument.getDocumentId())) {
                                         if(documentTypeObj.getDocument_type_id().equals(Constant.DOCUMENT_TYPE_MARK_SHEET_ID))
                                         {
-                                            System.out.println("1258");
                                             documentToDelete.setQualificationDetails(null);
                                         }
                                         documentToDelete.setIsArchived(true);
