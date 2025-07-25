089face57ac948c711f63fbd5096b67f440c393f
Raman <chhaya.chugh@avisoft.io>
2025-07-25 10:39:57 +0530

    changed the url to service provider and public dashboard

--- api/src/main/java/com/community/api/services/SharedUtilityService.java
+++ api/src/main/java/com/community/api/services/SharedUtilityService.java
@@ -2227,7 +2227,7 @@ public class SharedUtilityService {
         }
 
         text.append("\nTo view the latest changes, please visit:\n");
-        text.append("https://dev-next-am-public-ui.vercel.app/product-details/")
+        text.append("https://dev-next-am-public-ui.vercel.app/")
                 .append(updatedProduct.getId()).append("\n\n");
 
         text.append("Thank you,\nSystem Administrator");
