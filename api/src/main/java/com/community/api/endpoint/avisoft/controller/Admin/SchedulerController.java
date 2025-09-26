23ee5687aead606e49aa17f8f1e1cdae81f479e0
Raman <raman.kumar@avisoft.io>
2025-09-26 18:37:06 +0530

    changed db path in sceduler

--- api/src/main/java/com/community/api/endpoint/avisoft/controller/Admin/SchedulerController.java
+++ api/src/main/java/com/community/api/endpoint/avisoft/controller/Admin/SchedulerController.java
@@ -76,7 +76,7 @@ public class SchedulerController {
         );
 
         processBuilder.environment().put("PGPASSWORD", password);
-        processBuilder.directory(new File("C:/Program Files/PostgreSQL/13/bin"));
+        processBuilder.directory(new File("C:/Program Files/PostgreSQL/16/bin"));
 
         Schedule schedule = entityManager.find(Schedule.class, 3L);
         if (schedule == null) {
